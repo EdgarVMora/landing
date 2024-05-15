08015a71c775abbb3620a215ba46c524179f982e
Edgar Mora <cansiller.ed@gmail.com>
2024-05-15 00:19:46 -0600

    primera modificacion sectionfour

--- src/pages/SectionFour.jsx
+++ src/pages/SectionFour.jsx
@@ -2,6 +2,27 @@ function Testimonials(){
     return(
         <div className="anonymous_testimony">
             <h2>Testimonials</h2>
+            <div className="clients">
+                <div className="client">
+                    <h4>Client 1</h4>
+                    <p>Es flojo como una piedra, 
+                        pero sabe sacar la chamba
+                    </p>
+                </div>
+                <div className="client">
+                    <h4>Client 2</h4>
+                    <p>Tiene encanto, la mirada un poco
+                        perdida pero sabe lo que hace
+                    </p>
+                </div>
+                <div className="client">
+                    <h4>Client 3</h4>
+                    <p>Nidie me dijo lo que era trabajar
+                        con el, vaya sorpresa. 10/10
+                    </p>
+                </div>
+            </div>
+
         </div>
     )
 }
